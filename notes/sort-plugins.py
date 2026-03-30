2e87f8f763310dfe427e9cf8dda7d0813cc7be68
Isaac Riley <yelircaasi@proton.me>
2026-03-30 18:47:01 +0200

    format and lint old python scripts

--- notes/sort-plugins.py
+++ notes/sort-plugins.py
@@ -6,60 +6,61 @@ path = Path("/Users/ext_riley/repos/nvim-config/based/notes/plugins.json")
 
 d = json.loads(path.read_text())
 for x in d:
-        x["projects"] = sorted(set(x["projects"]))
-        if (x["decision"] == "rejected") and "reason" not in x:
-            x["reason"] = "none"
+    x["projects"] = sorted(set(x["projects"]))
+    if (x["decision"] == "rejected") and "reason" not in x:
+        x["reason"] = "none"
 decisions = (
-        "glean",
-        "vendored",
-        "configured",
-        "inConfig",
-        "selected",
-        "vendor",
-        "trying",
-        "next",
-        "sooner",
-        "pending",
-        "laterA",
-        "laterB",
-        "laterC",
-        "lang",
-        "laterD",
-        "later",
-        "extra",
-        "rejected",
-        "hack",
-    )
+    "glean",
+    "vendored",
+    "configured",
+    "inConfig",
+    "selected",
+    "vendor",
+    "trying",
+    "next",
+    "sooner",
+    "pending",
+    "laterA",
+    "laterB",
+    "laterC",
+    "lang",
+    "laterD",
+    "later",
+    "extra",
+    "rejected",
+    "hack",
+)
 sorter = dict(zip(decisions, range(20)))
 d.sort(key=lambda x: (sorter[x["decision"]], x["projects"]))
 c = Counter((x["decision"] for x in d))
 print(c)
 
 selected = set(
-        x.lower()
-        for x in 
-        Path("/Users/ext_riley/repos/nvim-config/testing/selected.txt").read_text().split("\n")
-    )
+    x.lower()
+    for x in Path("/Users/ext_riley/repos/nvim-config/testing/selected.txt")
+    .read_text()
+    .split("\n")
+)
 projects = []
 for x in d:
-        if (x["decision"] == "trying") and (x["link"].lower() not in selected):
-            print(x["link"])
-        # if x["decision"] != "pending":
-        projects.extend(set(x["projects"]))
+    if (x["decision"] == "trying") and (x["link"].lower() not in selected):
+        print(x["link"])
+    # if x["decision"] != "pending":
+    projects.extend(set(x["projects"]))
 
 
 for k, v in Counter(projects).most_common():
-        print(f"{k:<20} {v:>4}")
+    print(f"{k:<20} {v:>4}")
 
-    # links = {x["link"] for x in d}
-    # for link in (selected - links):
-    #     if link.startswith("h"):
-    #         print(link)
-    #         d.append({
-    #             "link": link,
-    #             "decision": "trying",
-    #             "projects": []
-    #         })
+# links = {x["link"] for x in d}
+# for link in (selected - links):
+#     if link.startswith("h"):
+#         print(link)
+#         d.append({
+#             "link": link,
+#             "decision": "trying",
+#             "projects": []
+#         })
 
 
 print()
@@ -78,7 +79,11 @@ print(c)
 
 
 def reformat(s: str) -> str:
-        return s.replace('[\n            "', '["').replace('"\n        ]', '"]').replace('",\n            "', '","')
+    return (
+        s.replace('[\n            "', '["')
+        .replace('"\n        ]', '"]')
+        .replace('",\n            "', '","')
+    )
 
 
 path.write_text(reformat(json.dumps(d, indent=4, ensure_ascii=False)))
