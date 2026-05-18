dbe880d738c9ae87d3f50063cc25c24f040ea09e
Isaac Riley <yelircaasi@proton.me>
2026-05-18 15:39:08 +0200

    add old nvim files from mac

---
+++ lua/options.lua
@@ -0,0 +1,4 @@
+vim.opt.number = true -- Show absolute line number on the current line
+vim.opt.relativenumber = true -- Show relative numbers on other lines
+vim.g.mapleader = " "
+vim.opt.termguicolors = true
---
+++ plugin-locations.lua
@@ -0,0 +1,5 @@
+M = {
+    ['oil.nvim'] = { path = "/nix/store/asdfsdfgkjasdhlkfjsadlkjalskh" },
+    ['blink.cmp'] = { path = "/nix/store/asdfsdfgkjasdhlkasdffjsadlkjalskh" },
+}
+return M
